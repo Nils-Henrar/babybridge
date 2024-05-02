5210945e3299cc143bbc73a1176590bdd797663e
Nils Henrar <nils.henrar@gmail.com>
2024-05-02 15:37:43 +0200

    gate pour les roles admin, worker et tutor

--- babybridge/app/Providers/AppServiceProvider.php
+++ babybridge/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        //admin gate
+
+        Gate::define('admin', function (User $user) {
+            return $user->roles->contains('role', 'admin');
+        });
+
+        //worker gate
+
+        Gate::define('worker', function (User $user) {
+            return $user->roles->contains('role', 'worker');
+        });
+
+        //tutor gate
+
+        Gate::define('tutor', function (User $user) {
+            return $user->roles->contains('role', 'tutor');
+        });
     }
 }
