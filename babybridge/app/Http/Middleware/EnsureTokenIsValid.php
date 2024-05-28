e040c062a95488bcddf0ff33c194abd6d661fd27
Nils Henrar <nils.henrar@gmail.com>
2024-05-28 15:45:08 +0200

    EnsureToken Update+ config axios dans bootstrapjs

--- babybridge/app/Http/Middleware/EnsureTokenIsValid.php
+++ babybridge/app/Http/Middleware/EnsureTokenIsValid.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class EnsureTokenIsValid
 {
@@ -18,14 +19,24 @@ class EnsureTokenIsValid
     {
         if (!$request->session()->has('authToken')) {
             return response()->json(['message' => 'Token not found in session'], 401);
+        }else{
+            // log pour dire que le token est dans la session
+            Log::info('Token is in session');
         }
 
         $token = $request->session()->get('authToken');
 
         // Optionnel : vérifier le token à partir d'un cookie
-        // $token = $request->cookie('authToken');
+        $token = $request->cookie('authToken');
 
         // Ici, vous pouvez ajouter une logique pour valider le token si nécessaire
+
+        if (empty($token)) {
+            return response()->json(['message' => 'Invalid token'], 401);
+        } else {
+            // log pour dire que le token est valide
+            Log::info('Token is valid');
+        }
 
         return $next($request);
     }
