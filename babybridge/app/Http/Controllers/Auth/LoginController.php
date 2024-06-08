e2e4695442adbc46516f41e1a91df09b1069feb2
Nils Henrar <nils.henrar@gmail.com>
2024-06-08 16:46:01 +0200

    redirection /home vers les dashboards distincts

--- babybridge/app/Http/Controllers/Auth/LoginController.php
+++ babybridge/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,20 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected function redirectTo()
+    {
+
+        if (auth()->user()->roles->contains('role', 'worker')) {
+            return route('worker.section.children');
+        } elseif (auth()->user()->roles->contains('role', 'admin')) {
+            return route('admin.user.index');
+        } elseif (auth()->user()->roles->contains('role', 'tutor')) {
+            return route('tutor.children.daily-journal');
+        }
+
+        // avec message flash
+        return redirect()->route('home')->with('error', 'Vous n\'avez pas la permission d\'accéder à cette page');
+    }
 
     /**
      * Create a new controller instance.
@@ -36,39 +50,39 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return $this->loggedOut($request) ?: redirect('/');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+
+        // Debug message to ensure authenticated method is being called
+        Log::info('User authenticated', ['user_id' => $user->id]);
+        // Generate token for the authenticated user
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        // Store the token in the session for use in the application
+        // session(['authToken' => $token]);
+
+        // Store the token in an HttpOnly, Secure cookie
+        cookie()->queue(cookie('authToken', $token, 60, null, null, true, true, false, 'Strict'));
+
+        if ($request->is('api/*')) {
+            return response()->json([
+                'message' => 'Login successful',
+                'token' => $token
+            ]);
         }
-
-        public function logout(Request $request)
-        {
-            $this->guard()->logout();
-
-            $request->session()->invalidate(); 
-
-            $request->session()->regenerateToken();
-
-            return $this->loggedOut($request) ?: redirect('/');
-        }
-
-        protected function authenticated(Request $request, $user)
-        {
-            // Generate token for the authenticated user
-            $token = $user->createToken('API Token')->plainTextToken;
-
-            // Store the token in the session for use in the application
-            session(['authToken' => $token]); 
-
-            // Store the token in an HttpOnly, Secure cookie
-            cookie()->queue(cookie('authToken', $token, 60, null, null, true, true, false, 'Strict'));
-
-            // Redirect based on user role
-            if ($user->roles->contains('role', 'worker')) {
-                return redirect()->route('worker.section.children');
-            } elseif ($user->roles->contains('role', 'admin')) {
-                return redirect()->route('admin.user.index');
-            } elseif ($user->roles->contains('role', 'tutor')) {
-                return redirect()->route('tutor.children.daily-journal');
-            }
-    
-            return redirect()->intended($this->redirectPath());
+        return redirect()->intended($this->redirectPath());
     }
 }
